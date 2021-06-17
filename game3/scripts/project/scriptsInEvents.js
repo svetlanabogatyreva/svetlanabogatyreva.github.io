53c1775677026ec2e258f861c822a47b90ddd268
Svetlana Bogatyreva <59484400+svetlanabogatyreva@users.noreply.github.com>
2021-06-17 12:33:49 +0300

    Add files via upload

---
+++ game3/scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async QuizEvents_Event19_Act8(runtime, localVars)
+		{
+			platformProvider.showInterstitial()
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
