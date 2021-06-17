aa38a6da3b026af3d7c9957e23e605c99738cbed
Svetlana Bogatyreva <59484400+svetlanabogatyreva@users.noreply.github.com>
2021-06-17 14:08:50 +0300

    Add files via upload

---
+++ game3/scripts/project/scriptsInEvents.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async QuizEvents_Event19_Act8(runtime, localVars)
+		{
+			platformProvider.showInterstitial()
+		},
+
+		async StartEvents_Event3_Act1(runtime, localVars)
+		{
+			let options = {
+				message: 'Космос тест. Кто ты? 😎 Знаток или новичок?',
+				link: 'https://vk.com/app7882145'
+			}
+			platformProvider.share(options)
+		},
+
+		async StartEvents_Event4_Act1(runtime, localVars)
+		{
+			platformProvider.addToFavorites()
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
